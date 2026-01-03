783a5d44daf8b1f09073111d296a01f2ed9698e9
Ye Pyae Aung <yepyaeaung1311999@gmail.com>
2026-01-03 19:32:23 +0630

    YP: add padding bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-sm:pb-14`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-sm:pb-14 pb-20`}
       >
         {children}
         <Navbar />
